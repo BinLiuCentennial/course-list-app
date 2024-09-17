fe39c1fb759849abe226de3386e990d30afe1cde
BinLiuCentennial <133721923+BinLiuCentennial@users.noreply.github.com>
2024-09-16 20:01:58 -0400

    Course List

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
+const App = () => {
+
+  // React Code
+  //return React.createElement('h1', {title:'Professor: Krunal'}, 'Hello Friends, Welcome to COMP229');
+
+  // JSX code
+  //return <h1 title="Professor: Krunal">Hello Friends, Welcome to COMP229</h1> 
+  // JSX code: it looks like HTML but not HTML code, 
+  // it converts code into the JS and basic HTML.
+  return (
+    <div className="course-info">
+      <h2>Course List</h2>
+        <ul className="course-list">
+          <li>Comp229</li>
+          <li>Comp246</li>
+          <li>Comp225</li>
+        </ul>
+    </div>
+  );
+};
+
+/*
 function App() {
   return (
     <div className="App">
@@ -20,6 +43,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
